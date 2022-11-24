842b7cb054e0c2fa57e5b6ba810ff20825006a01
jakl42 <zan.jaklic42@gmail.com>
2022-11-24 10:55:58 +0100

    Added User model, database settings and controller

---
+++ Repository/Repository/Models/User.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Repository.Models
+{
+    public class User
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long Id { get; set; }
+        [Required]
+        public string? UserName { get; set; }
+        [Required]
+        public string? FullName { get; set; }
+        [Required]
+        [EmailAddress]
+        public string? Email { get; set; }
+        [Required]
+        [Phone]
+        public string? MobileNumber { get; set; }
+        [Required]
+        public string? Language { get; set; }
+        [Required]
+        public string? Culture { get; set; }
+        [Required]
+        public string? Password { get; set; }
+    }
+}
